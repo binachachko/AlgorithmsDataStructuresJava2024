74c2c496b2afa6874e4e2bb878122feeb952a86e
Bina <binachachko@gmail.com>
2024-10-01 14:54:34 +0300

    Linked list task edited

--- LinkedList/ListNode.java
+++ LinkedList/ListNode.java
@@ -1,7 +1,43 @@
-public class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+class ListNode {
+    int val;
+    ListNode next;
+
+    private boolean isEmpty = true;
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+        this.isEmpty = false;
+    }
+
+    public boolean isEmpty() {
+        return isEmpty;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+        this.isEmpty = false;
+    }
+
+
+    // Метод для виведення списку у вигляді рядка з дужками
+    @Override
+    public String toString() {
+        if (this.isEmpty) {
+            return "[]";
+        }
+
+        StringBuilder sb = new StringBuilder("[");
+        ListNode current = this;
+        while (current != null) {
+            sb.append(current.val);
+            current = current.next;
+            if (current != null) {
+                sb.append(",");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -2,35 +2,19 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
-        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
-        ListNode mergedList1 = solution.mergeTwoLists(list1, list2);
-        printList(mergedList1);
+        final ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        final ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        final ListNode mergedList1 = solution.mergeTwoLists(list1, list2);
+        System.out.println(mergedList1);
 
-        ListNode list3 = null;
-        ListNode list4 = null;
-        ListNode mergedList2 = solution.mergeTwoLists(list3, list4);
-        printList(mergedList2);
+        final ListNode list3 = new ListNode();
+        final ListNode list4 = new ListNode();
+        final ListNode mergedList2 = solution.mergeTwoLists(list3, list4);
+        System.out.println(mergedList2);
 
-        ListNode list5 = null;
-        ListNode list6 = new ListNode(0);
-        ListNode mergedList3 = solution.mergeTwoLists(list5, list6);
-        printList(mergedList3);
-    }
-
-    public static void printList(ListNode node) {
-        if (node == null) {
-            System.out.println("[]");
-            return;
-        } else {
-            System.out.print("[");
-
-            while (node != null) {
-                System.out.print(node.val);
-                node = node.next;
-                if (node != null) System.out.print(",");
-            }
-            System.out.println("]");
-        }
+        final ListNode list5 = new ListNode();
+        final ListNode list6 = new ListNode(0);
+        final ListNode mergedList3 = solution.mergeTwoLists(list5, list6);
+        System.out.println(mergedList3);
     }
 }

3736d2821efa74a6aa9c75e0a1f4ebd2c05e653f
Bina <binachachko@gmail.com>
2024-10-07 20:21:19 +0300

    HashMap task edited

--- HashMap/Main.java
+++ HashMap/Main.java
@@ -7,7 +7,7 @@ public class Main {
         schedule.addClass("MATH101", "Mathematics", LocalTime.parse("09:00"));
         schedule.addClass("PHY102", "Physics", LocalTime.parse("11:00"));
         schedule.addClass("CHE103", "Chemistry", LocalTime.parse("12:00"));
-        schedule.addClass("LIT103", "Literature", LocalTime.parse("13:00"));
+        schedule.addClass("LIT104", "Literature", LocalTime.parse("13:00"));
 
         System.out.println("\nAll classes: ");
         schedule.printAllClasses();
@@ -18,6 +18,14 @@ public class Main {
         schedule.removeClass("PHY102");
 
         System.out.println("\nAll classes after removal:");
+        schedule.printAllClasses();
+
+        System.out.println("\nAdd class BIO105 with incorrect time: ");
+        schedule.addClass("BIO105", "Biology", LocalTime.parse("18:00"));
+
+        System.out.println("\nAdd correct class BIO105: ");
+        schedule.addClass("BIO105", "Biology", LocalTime.parse("14:00"));
+        System.out.println("\nAll classes after addition: ");
         schedule.printAllClasses();
     }
 }
--- HashMap/Schedule.java
+++ HashMap/Schedule.java
@@ -6,6 +6,13 @@ public class Schedule {
     public HashMap<String, Class> classCodeAndClass = new HashMap<>();
 
     public void addClass(String classCode, String subjectName, LocalTime time) {
+        LocalTime startTime = LocalTime.of(8, 0);
+        LocalTime endTime = LocalTime.of(17, 0);
+        if (time.isBefore(startTime) || time.isAfter(endTime)) {
+            System.out.println("Class time must be between 08:00 and 17:00. Invalid time: " + time);
+            return;
+        }
+
         Class newClass = new Class(classCode, subjectName, time);
         classCodeAndClass.put(classCode, newClass);
     }
